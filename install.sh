d3d4470dedb2ac2d6f6e0ea7a82485faa5db4f05
Josh Horowitz <joshua.m.horowitz@gmail.com>
2017-10-27 13:21:48 -0400

    Updated install.sh to get sudo power

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-echo "https://raw.githubusercontent.com/jhorowitz/mac-install-scripts/master/homebrew.sh
+echo "https://raw.githubusercontent.com/jhorowitz/mac-install-scripts/master/get-sudo.sh
+https://raw.githubusercontent.com/jhorowitz/mac-install-scripts/master/homebrew.sh
 https://raw.githubusercontent.com/jhorowitz/mac-install-scripts/master/essentials.sh
 https://raw.githubusercontent.com/jhorowitz/mac-install-scripts/master/vim.sh
 https://raw.githubusercontent.com/jhorowitz/mac-install-scripts/master/vim-configure.sh" | xargs -I {} curl -s {}
